0ecac618726f5d8fdf00e81999e72f5a61495a6d
deb <vinhlq@hotmail.com>
2019-05-03 14:47:02 +0700

    ...

--- git_push.sh
+++ git_push.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-cd client/api
-git add .
-git commit -m "..."
-GIT_SSH_COMMAND='ssh -i ../../ssh.rsa.pem' git push origin master
-
-cd ../..
 git add .
 git commit -m "..."
 GIT_SSH_COMMAND='ssh -i ssh.rsa.pem' git push origin master
